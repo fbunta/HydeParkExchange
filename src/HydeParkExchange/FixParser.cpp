6bba57ebf9d5a932942df18c2c2c9a21dbe59b99
Om Suchak <30605707+omsuchak@users.noreply.github.com>
2023-05-24 20:58:10 -0500

    Update FixParser.cpp

--- src/HydeParkExchange/FixParser.cpp
+++ src/HydeParkExchange/FixParser.cpp
@@ -5,75 +5,61 @@
 #include <unordered_map>
 #include<sstream>
 #include <fstream>
+#include "ctre.hpp"
 
-std::unordered_map<int, std::string> getFieldNames() {
-  std::unordered_map<int, std::string> fieldNames;
+static std::unordered_map<int, std::string> fieldNames = {
+  {1, "Account"},
+  {6, "AvgPx"},
+  {8, "BeginString"},
+  {10, "Checksum"},
+  {11, "ClOrdID"},
+  {14, "CumQty"},
+  {15, "Currency"},
+  {17, "ExecID"},
+  {20, "ExecTransType"},
+  {22, "IDSource"},
+  {29, "LastQty"},
+  {30, "LastMkt"},
+  {31, "LastPx"},
+  {32, "LastShares"},
+  {34, "MsgSeqNum"},
+  {35, "MessageType"},
+  {37, "OrderID"},
+  {38, "OrderQty"},
+  {39, "OrdStatus"},
+  {40, "OrdType"},
+  {41, "OrigClOrdID"},
+  {44, "Price"},
+  {48, "SecurityID"},
+  {49, "SenderCompID"},
+  {50, "SenderSubID"},
+  {52, "SendingTime"},
+  {54, "Side"},
+  {55, "Symbol"},
+  {56, "TargetCompID"},
+  {57, "TargetSubID"},
+  {59, "TimeInForce"},
+  {60, "TransactTime"},
+  {75, "TradeDate"},
+  {76, "TrdMatchID"},
+  {109, "ClientID"},
+  {150, "ExecType"},
+  {151, "LeavesQty"},
+  {207, "SecurityExchange"},
+  {8201, "Field1"},
+};
 
-  fieldNames[1] = "Account";
-  fieldNames[6] = "AvgPx";
-  fieldNames[8] = "BeginString";
-  fieldNames[10] = "Checksum";
-  fieldNames[11] = "ClOrdID";
-  fieldNames[14] = "CumQty";
-  fieldNames[15] = "Currency";
-  fieldNames[17] = "ExecID";
-  fieldNames[20] = "ExecTransType";
-  fieldNames[22] = "IDSource";
-  fieldNames[29] = "LastQty";
-  fieldNames[30] = "LastMkt";
-  fieldNames[31] = "LastPx";
-  fieldNames[32] = "LastShares";
-  fieldNames[34] = "MsgSeqNum";
-  fieldNames[35] = "MessageType"; 
-  fieldNames[37] = "OrderID";
-  fieldNames[38] = "OrderQty";
-  fieldNames[39] = "OrdStatus";
-  fieldNames[40] = "OrdType";
-  fieldNames[41] = "OrigClOrdID";
-  fieldNames[44] = "Price"; 
-  fieldNames[48] = "SecurityID";
-  fieldNames[49] = "SenderCompID";
-  fieldNames[50] = "SenderSubID";
-  fieldNames[52] = "SendingTime";
-  fieldNames[54] = "Side";
-  fieldNames[55] = "Symbol";
-  fieldNames[56] = "TargetCompID";
-  fieldNames[57] = "TargetSubID";
-  fieldNames[59] = "TimeInForce";
-  fieldNames[60] = "TransactTime";
-  fieldNames[75] = "TradeDate";
-  fieldNames[76] = "TrdMatchID";
-  fieldNames[109] = "ClientID";
-  fieldNames[150] = "ExecType";
-  fieldNames[151] = "LeavesQty";
-  fieldNames[207] = "SecurityExchange";
-  fieldNames[8201] = "Field1";
-
-  return fieldNames;
+void parseFixMessage(const std::string_view &fixMessage) {
+    
+    //auto matches = ctre::match<"\x01([^=]+)=([^\x01]+)">(fixMessage);
+    for (auto match : ctre::range<"\x01([^=]+)=([^\x01]+)">(fixMessage)) {
+        std::string fieldName = std::string(match.get<1>().to_view());
+        std::cout << fieldNames[std::stoi(fieldName)]
+                  << " : " << match.get<2>().to_view() << "\n";
+    }
 }
 
-// Function to parse a FIX message using regex
-void parseFixMessage(const std::string &fixMessage) {
-  std::unordered_map<int, std::string> fieldNames = getFieldNames();
-
-  // note the \x01 is an SOH character (some people visualize with a pipe character)
-  std::regex pattern("\x01([^=]+)=([^\x01]+)");
-
-  std::sregex_iterator it(fixMessage.begin(), fixMessage.end(), pattern);
-  std::sregex_iterator end;
-
-  while (it != end) {
-    std::smatch match = *it;
-    std::string tag = match.str(1);
-    std::string value = match.str(2);
-    std::string fieldName = fieldNames[std::stoi(tag)];
-
-    std::cout << fieldName << "=" << value << std::endl;
-    it++;
-  }
-}
-
-int main_()
+int main()
 {
     std::ifstream inputFile("FIX.txt");
     if (!inputFile.is_open()) {
@@ -87,6 +73,9 @@ int main_()
         std::cout << " " << std::endl;
     }
     inputFile.close();
+    
+    // To test CTRE
+    parseFixMessage("8=FIX.4.29=035=D49=83019956=AZKJ34=057=362052=20150406-12:17:2711=0c968e69-c3ff-4f9f-bc66-9e5ebccd980741=e0568b5c-8bb1-41f0-97bf-5eed32828c241=90964630055=SJM48=46428843022=154=138=7570040=115=USD59=060=20150406-12:17:278201=1207=P10=0");
 
     return 0;
 }
